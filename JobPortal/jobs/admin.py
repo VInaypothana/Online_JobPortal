a074e0c7494656ec75b43360a672ae6d2a197122
uday <97800274+udaypothana@users.noreply.github.com>
2022-12-07 11:24:51 +0530

    Initial commit

---
+++ JobPortal/jobs/admin.py
@@ -0,0 +1,11 @@
+
+# Register your models here.
+
+from django.contrib import admin
+from . models import *
+
+admin.site.register(Applicant)
+admin.site.register(Company)
+admin.site.register(Job)
+admin.site.register(Application)
+
